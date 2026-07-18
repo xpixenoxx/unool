12a8c1925f8e7476f856ec1720ce18afd7bbfa35
xpixenoxx <xpixenox@gmail.com>
2026-07-18 23:35:39 +0530

    Debug: Add test header to next.config.js

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
           { key: 'X-DNS-Prefetch-Control', value: 'on' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+          { key: 'X-Debug-Middleware', value: 'middleware-ran' },
         ],
       },
     ];
