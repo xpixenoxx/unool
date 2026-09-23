4749a0467ddf130a67333c331a3205b900e342df
Heman <heman@unool.co>
2026-09-24 00:47:39 +0530

    fix(ssr): add use-client boundary to IdentityTemplate to fix rendering exception

--- components/profile/templates/persona/IdentityTemplate.tsx
+++ components/profile/templates/persona/IdentityTemplate.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import type { TemplateProps } from '@/components/profile/templates/types';
 import { getTemplateById } from '@/components/profile/templates/registry';
